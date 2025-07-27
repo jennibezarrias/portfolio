49c98c640669f12a6fc2d7d7e34405d2df4fd629
Jenni <jennibezarriass@gmail.com>
2025-07-27 20:27:13 -0300

    pagina 404

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
